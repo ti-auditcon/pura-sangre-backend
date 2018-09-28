86e0ac152f4b2d4946aa7633bb94bf8779c34e82
boryeira <jmanuel.jorquera@gmail.com>
2018-09-28 11:50:43 -0300

    update routes

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ Route::post('login', 'API\Auth\AuthController@login');
 
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('profile', 'API\Users\UserController@profile');
+    Route::get('profile', 'API\Users\UserController@profile');
     Route::post('logout', 'API\Auth\AuthController@logout');
 });
