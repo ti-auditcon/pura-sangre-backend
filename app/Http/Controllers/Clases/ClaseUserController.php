12e8c6b8a633bced9dee14e57f5d0df93a151714
unknown <raulberrios8@gmail.com>
2018-10-16 11:59:45 -0300

    optional --- - -

--- app/Http/Controllers/Clases/ClaseUserController.php
+++ app/Http/Controllers/Clases/ClaseUserController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Clases;
 
-use Session;
-use Redirect;
-use App\Models\Users\User;
-use App\Models\Clases\Clase;
-use Illuminate\Http\Request;
-use App\Models\Clases\Reservation;
 use App\Http\Controllers\Controller;
+use App\Models\Clases\Clase;
+use App\Models\Clases\Reservation;
+use App\Models\Plans\PlanUser;
+use App\Models\Users\User;
+use Illuminate\Http\Request;
+use Redirect;
+use Session;
 
 class ClaseUserController extends Controller
 {
@@ -40,17 +41,21 @@ class ClaseUserController extends Controller
      */
     public function store(Request $request, Clase $clase)
     {
+        $plan_user = PlanUser::where('plan_status_id', 1)->where('user_id', $request->user_id)->get();
         $inclase = $this->inClass($clase, $request);
         if (count($inclase) == 0) {
+            if ($plan_user->plan_id == 5) {
+                $plan_user->counter =  $plan_user->counter + 1;
+            }
             Reservation::create(array_merge($request->all(), [
-                    'clase_id' => $clase->id,
-                    'reservation_status_id' => 1
+                'clase_id' => $clase->id,
+                'reservation_status_id' => 1
             ]));
             Session::flash('success','Usuario agregado');
             return Redirect::back();
         }
         else{
-            Session::flash('warning','El usuario ya esta en esta clase');
+            Session::flash('warning','El usuario ya está en esta clase');
             return Redirect::back();
         }
     }
@@ -102,6 +107,12 @@ class ClaseUserController extends Controller
         return Redirect::back();
     }
 
+    /**
+     * [inclass description]
+     * @param  [type] $clase   [description]
+     * @param  [type] $request [description]
+     * @return [collection] [description]
+     */
     public function inclass($clase, $request){
         $consulta = Reservation::where('clase_id', $clase->id)
                 ->where('user_id', $request->user_id)->get();
