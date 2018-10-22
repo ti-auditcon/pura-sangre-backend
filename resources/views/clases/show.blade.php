7d9f08d106a9ea89f06f4fcab0f9ba00e59cec08
unknown <raulberrios8@gmail.com>
2018-10-22 10:32:28 -0300

    fix avatar view

--- resources/views/clases/show.blade.php
+++ resources/views/clases/show.blade.php
@@ -171,7 +171,7 @@
               <tr>
                 <td>
                   <a class="media-img" href="javascript:;">
-                      <img class="img-circle" src="{{url('/storage/users/'.$reservation->user->avatar.'.jpg')}}" alt="image" width="54">
+                      <img class="img-circle" src="{{url('/storage/users/'.$usuario->avatar.'.jpg')}}" alt="image" width="54">
                   </a>
                 @if($usuario->status_user_id == 1 )
                   <span class="badge-success badge-point"></span>
