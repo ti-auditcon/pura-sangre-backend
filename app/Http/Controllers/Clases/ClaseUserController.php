b0bff2b05c383b79b4be4d30e8fba9f5d06e14a0
unknown <raulberrios8@gmail.com>
2018-11-12 13:56:13 -0300

    userclas controller

--- app/Http/Controllers/Clases/ClaseUserController.php
+++ app/Http/Controllers/Clases/ClaseUserController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers\Clases;
 
-use App\Http\Controllers\Controller;
-use App\Models\Clases\Clase;
-use App\Models\Clases\Reservation;
-use App\Models\Plans\PlanUser;
-use App\Models\Plans\PlanUserPeriod;
-use App\Models\Users\User;
 use Auth;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Redirect;
 use Session;
+use Redirect;
+use Carbon\Carbon;
+use App\Models\Users\User;
+use App\Models\Clases\Clase;
+use Illuminate\Http\Request;
+use App\Models\Plans\PlanUser;
+use App\Models\Clases\Reservation;
+use App\Http\Controllers\Controller;
+use App\Models\Plans\PlanUserPeriod;
 
 class ClaseUserController extends Controller
 {
@@ -25,21 +25,7 @@ class ClaseUserController extends Controller
                 Session::flash('warning', $response);
                 return Redirect::back();
             }
-        if(count($planusers) != 0){
-            foreach ($planusers as $planuser) {
-                foreach ($planuser->plan_user_periods as $pup) {
-                    if ($date_class->between(Carbon::parse($pup->start_date), Carbon::parse($pup->finish_date))) {
-                        $period_plan = $pup; 
-                    }
-                }
-            }
-            if (!Auth::user()->hasRole(1)) {
-                if ($period_plan->counter <= 0) {
-                    Session::flash('warning', 'Ya ha ocupado o reservado todas sus clases de este mes de su plan actual');
-                    return Redirect::back();
-                }
-            }
-        }else{
+        if(count($planusers) == 0){
             if (Auth::user()->hasRole(1)) {
                 Reservation::create(array_merge($request->all(), [
                     'clase_id' => $clase->id,
@@ -51,36 +37,33 @@ class ClaseUserController extends Controller
                 Session::flash('warning', 'No tienes ningun plan activo');
                 return Redirect::back();
             }
-        }
-  
-        if ($clase->date < toDay()->format('Y-m-d')) {
-            Session::flash('warning','No puede tomar una clase de un día anterior a hoy');
-            return Redirect::back();
-        }
-        elseif ($clase->date > toDay()->format('Y-m-d')) {
-            $period_plan->update(['counter' => $period_plan->counter - 1]);
-            Reservation::create(array_merge($request->all(), [
-                'clase_id' => $clase->id,
-                'reservation_status_id' => 1
-            ]));
-            Session::flash('success','Agregado correctamente a la clase');
-            return Redirect::back();
-        }
-        else {
-            $class_hour = Carbon::parse($clase->start_at);
-            $diff_mns = $class_hour->diffInMinutes(now()->format('H:i'));
-            if ((now()->format('H:i') > $class_hour) || (now()->format('H:i') < $class_hour && $diff_mns < 40)) {
-                Session::flash('warning', 'Ya no se puede tomar la clase');
-                return Redirect::back();
-            }else{
-                $period_plan->update(['counter' => $period_plan->counter - 1]);
-                Reservation::create(array_merge($request->all(), [
-                    'clase_id' => $clase->id,
-                    'reservation_status_id' => 1
-                ]));
-                Session::flash('success','Agregado correctamente a la clase');
+        }else{
+            foreach ($planusers as $planuser) {
+                foreach ($planuser->plan_user_periods as $pup) {
+                    if ($date_class->between(Carbon::parse($pup->start_date), Carbon::parse($pup->finish_date))) {
+                        $period_plan = $pup; 
+                    }
+                }
+            }
+
+            $response = $this->adminReserve($request, $period_plan);
+            if ($response != null) {
+                Session::flash('success', $response);
                 return Redirect::back();
             }
+
+            $response = $this->userBadReserve($clase, $period_plan);
+            if ($response != null) {
+                Session::flash('warning', $response);
+                return Redirect::back();
+            }
+
+            $response = $this->userReserve($clase, $period_plan, $request);
+            if ($response != null) {
+                Session::flash('warning', $response);
+                return Redirect::back();
+            }
+
         }
     }
 
@@ -105,6 +88,8 @@ class ClaseUserController extends Controller
                     }
                 }
             }
+        }elseif (count($planusers) == 0) {
+            # code...
         }
 
         if ($clase->date < toDay()->format('Y-m-d')) {
@@ -154,65 +139,59 @@ class ClaseUserController extends Controller
         return $response;
     }
 
+    private function adminReserve($request, $period_plan)
+    {
+        $response = null;
+        if (Auth::user()->hasRole(1)) {
+            if ($period_plan == null || $period_plan->counter <= 0) {
+                Reservation::create(array_merge($request->all(), [
+                    'clase_id' => $clase->id,
+                    'reservation_status_id' => 1
+                ]));
+                $response = 'Agregado correctamente a la clase';
+            }
+        }
+        return $response;
+    }
+
+    private function userBadReserve($period_plan, $clase)
+    {
+        $badResponse = null;
+        if ($period_plan->counter <= 0) {
+            $badResponse = 'Ya ha ocupado o reservado todas sus clases de este mes de su plan actual');
+        }
+        elseif ($clase->date < toDay()->format('Y-m-d')) {
+            $badResponse = 'No puede tomar una clase de un día anterior a hoy');
+        }
+        else {
+            $class_hour = Carbon::parse($clase->start_at);
+            $diff_mns = $class_hour->diffInMinutes(now()->format('H:i'));
+            if ((now()->format('H:i') > $class_hour) || (now()->format('H:i') < $class_hour && $diff_mns < 40)) {
+                $badResponse = 'Ya no se puede tomar la clase';
+            }
+        }  
+        return $badResponse;
+    }
+
+    private function userReserve($period_plan, $request, $clase)
+    {
+        $response = null;
+        if ($clase->date > toDay()->format('Y-m-d')) {
+            $period_plan->update(['counter' => $period_plan->counter - 1]);
+            Reservation::create(array_merge($request->all(), [
+                'clase_id' => $clase->id,
+                'reservation_status_id' => 1
+            ]));
+            $response = 'Agregado correctamente a la clase';
+        }
+        elseif ($clase->date == toDay()->format('Y-m-d')) {
+            $period_plan->update(['counter' => $period_plan->counter - 1]);
+            Reservation::create(array_merge($request->all(), [
+                'clase_id' => $clase->id,
+                'reservation_status_id' => 1
+            ]));
+            $response = 'Agregado correctamente a la clase';
+        }
+        return $response;
+    }
 }
-    // private function hasTwelvePlan($planuser)
-    // {
-    //     $responseTwo = null;
-    //     if ($planuser->plan->class_numbers == 12 && $planuser->counter >= 12 && $planuser->plan->plan_period_id == 1) {
-    //         $responseTwo = 'No puede reservar, ya ha ocupado o reservado sus 12 clases del plan 12 clases mensual';
-    //     }
-    //     elseif ($planuser->plan->class_numbers == 12 && $planuser->counter >= 12 && $planuser->plan->plan_period_id == 3) {
-    //         $responseTwo = 'Ya ha ocupado o reservado sus 12 clases de este mes del plan trimestral';
-    //     }
-    //     elseif ($planuser->plan->class_numbers == 12 && $planuser->counter >= 12 && $planuser->plan->plan_period_id == 5) {
-    //         $responseTwo = 'Ya ha ocupado o reservado sus 12 clases de este mes del plan semestral';
-    //     }
-    //     elseif ($planuser->plan->class_numbers == 12 && $planuser->counter >= 12 && $planuser->plan->plan_period_id == 6) {
-    //         $responseTwo = 'Ya ha ocupado o reservado sus 12 clases de este mes del plan anual';
-    //     }
-
-    //     return $responseTwo;
-    // }
-    // 
-    
-        // $responseTwo = $this->hasClassesLeft($planuser);
-        //     if ($responseTwo != null) {
-        //         Session::flash('warning', $responseTwo);
-        //         return Redirect::back();
-        //     }
-        //     
-        
-
-          // if (!Auth::user()->hasRole(1)) {
-        //     if ($planuser->counter <= 0) {
-        //     Session::flash('warning', 'Ya ha ocupado o reservado todas sus clases de este mes de su plan actual');
-        //     return Redirect::back();
-        //     }
-        // }
-
-
-        // obtener periodos de los planes
-        // recorrer periodos 
-        // la fecha de reserva esta dentro de un periodo
-        // ubicar periodo  
-        // usuario tiene cupos en el periodo
-    
-      // foreach ($periodos as $periodo) {
-        //     if ($date_class->between(Carbon::parse($periodo->start_date), Carbon::parse($periodo->finish_date))) {
-        //         $period_plan[] = $periodo; 
-        //     }
-        // }
-    
-    // /**
-    //  * [hasClassesLeft description]
-    //  * @param  [type]  $planuser [description]
-    //  * @return String  [description]
-    //  */
-    // private function hasClassesLeft($planuser)
-    // {
-    //     $responseThree = null;
-    //     if ($planuser->counter <= 0) {
-    //        $responseThree = 'Ya ha ocupado o reservado todas sus clases de este mes del plan'; 
-    //     }
-    //     return $responseThree;
-    // }
