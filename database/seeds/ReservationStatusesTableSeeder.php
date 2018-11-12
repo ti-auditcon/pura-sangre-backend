98f677ac5b2410a534444d4cf10c8314f5889a7b
boryeira <jmanuel.jorquera@gmail.com>
2018-11-12 16:23:24 -0300

    migration reservation statuses add perdida y consumida

--- database/seeds/ReservationStatusesTableSeeder.php
+++ database/seeds/ReservationStatusesTableSeeder.php
@@ -12,27 +12,41 @@ class ReservationStatusesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('reservation_statuses')->delete();
-        
+
         \DB::table('reservation_statuses')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'reservation_status' => 'Pendiente',
                 'created_at' => '2018-10-25 12:29:47',
                 'updated_at' => '2018-10-25 12:29:47',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
-                'reservation_status' => 'Confirmado',
+                'reservation_status' => 'Confirmada',
+                'created_at' => '2018-10-25 12:29:47',
+                'updated_at' => '2018-10-25 12:29:47',
+            ),
+            2 =>
+            array (
+                'id' => 3,
+                'reservation_status' => 'Consumida',
+                'created_at' => '2018-10-25 12:29:47',
+                'updated_at' => '2018-10-25 12:29:47',
+            ),
+            3 =>
+            array (
+                'id' => 4,
+                'reservation_status' => 'Perdida',
                 'created_at' => '2018-10-25 12:29:47',
                 'updated_at' => '2018-10-25 12:29:47',
             ),
         ));
-        
-        
+
+
     }
 }
