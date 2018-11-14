a78d85f2d2d839b4cb2065dde7afa9bcc5f349e7
boryeira <jmanuel.jorquera@gmail.com>
2018-11-14 11:34:04 -0300

    fsdf

--- database/factories/PlanFactory.php
+++ database/factories/PlanFactory.php
@@ -8,20 +8,19 @@ use App\Models\Plans\PlanUser;
 use App\Models\Bills\Installment;
 use App\Models\Bills\PaymentStatus;
 
-<<<<<<< HEAD
-=======
+
 // $factory->define(Plan::class, function (Faker $faker) {
 //     return [
 //       'plan' => $faker->word,
 //       'class_numbers' => $faker->numberBetween($min = 12, $max = 24),
 //     ];
 // });
->>>>>>> e12be5ec10fd4bd9c056dd5e32d53096ee9f21f2
+
 
 $factory->define(PlanUser::class, function (Faker $faker) {
 //
   $plan = Plan::inRandomOrder()->first();
-<<<<<<< HEAD
+
 
   $starts_at = Carbon::createFromTimestamp($faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now')->getTimeStamp());
   $ends_at= Carbon::createFromFormat("Y-n-j G:i:s", $starts_at)->addMonths($plan->plan_period->period_number ?? 1 );
@@ -36,7 +35,7 @@ $factory->define(PlanUser::class, function (Faker $faker) {
       $plan_status_id = '4';
     }
 
-=======
+
   $starts_at = Carbon::createFromTimestamp($faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now')->getTimeStamp());
   $ends_at= Carbon::createFromFormat("Y-n-j G:i:s", $starts_at)->addMonths($plan->plan_period->period_number);
 
@@ -50,7 +49,7 @@ $factory->define(PlanUser::class, function (Faker $faker) {
       $plan_status_id = '4'
     }
 
->>>>>>> e12be5ec10fd4bd9c056dd5e32d53096ee9f21f2
+
   }
 
     return [
@@ -63,7 +62,7 @@ $factory->define(PlanUser::class, function (Faker $faker) {
     ];
 });
 
-<<<<<<< HEAD
+
 // $factory->define(Plan::class, function (Faker $faker) {
 //     return [
 //       'plan' => $faker->word,
@@ -86,8 +85,7 @@ $factory->define(PlanUser::class, function (Faker $faker) {
 //     ];
 // });
 //
-=======
->>>>>>> e12be5ec10fd4bd9c056dd5e32d53096ee9f21f2
+
 // $factory->define(Installment::class, function (Faker $faker) {
 //     return [
 //         'bill_id' => Bill::all()->random()->id,
