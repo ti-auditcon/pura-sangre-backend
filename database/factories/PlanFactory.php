642209008a6a5835094d302fd633a78fd1bb5731
boryeira <jmanuel.jorquera@gmail.com>
2018-11-14 11:00:41 -0300

    uikuy

--- database/factories/PlanFactory.php
+++ database/factories/PlanFactory.php
@@ -17,17 +17,30 @@ use App\Models\Bills\PaymentStatus;
 
 $factory->define(PlanUser::class, function (Faker $faker) {
 //
-// $starts_at = Carbon::createFromTimestamp($faker->dateTimeBetween($startDate = '-1 months', $endDate = 'now')->getTimeStamp());
-// $ends_at= Carbon::createFromFormat("Y-n-j G:i:s", $starts_at)->addMonths($faker->numberBetween( 1, 2, 3));
-//
-//     return [
-//       'start_date' => $starts_at,
-//       'finish_date' => $ends_at,
-//       'counter' => 5,
-//       'plan_status_id' => $faker->randomElement($array = array ('1', '2', '3', '4', '5')),
-//       // 'plan_status_id' => 1,
-//       'plan_id' => Plan::all()->random()->id,
-//     ];
+  $plan = Plan::inRandomOrder()->first();
+  $starts_at = Carbon::createFromTimestamp($faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now')->getTimeStamp());
+  $ends_at= Carbon::createFromFormat("Y-n-j G:i:s", $starts_at)->addMonths($plan->plan_period->period_number);
+
+  if($starts_at >= today()){
+    $plan_status_id = '3'
+  } else {
+    if($ends_at >= today())
+    {
+      $plan_status_id = '1'
+    } else {
+      $plan_status_id = '4'
+    }
+
+  }
+
+    return [
+      'start_date' => $starts_at,
+      'finish_date' => $ends_at,
+      'counter' => $plan->class_numbers,
+      'plan_status_id' => $plan_status_id,
+      'user_id' => 1,
+      'plan_id' => $plan->id,
+    ];
 });
 
 // $factory->define(Installment::class, function (Faker $faker) {
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -86,8 +86,8 @@ class DatabaseSeeder extends Seeder
 
       factory(User::class, 50)->create()->each(function ($u)
       {
-          factory(PlanUser::class, 1)->create(['user_id' => $u->id ]);
-          factory(Reservation::class, 20)->create(['user_id' => $u->id ]);
+          factory(PlanUser::class, 10)->create(['user_id' => $u->id ]);
+          //factory(Reservation::class, 20)->create(['user_id' => $u->id ]);
       });
       $this->call(PlanUserTableSeeder::class);
       $this->call(RoleUserTableSeeder::class);
