f05b60d7184444222386fda7a38074c48382188c
Juan <jberrioscifuentes@gmail.com>
2018-12-20 11:32:46 -0300

    =

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
+
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Correo') }}</label>
 
                             <div class="col-md-6">
