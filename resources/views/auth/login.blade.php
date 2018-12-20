60242e39c22888efcaea9a5e808103f740aa3862
unknown <raulberrios8@gmail.com>
2018-12-20 11:35:46 -0300

    Revert "Merge branch 'developer' into raul"
    This reverts commit fe1411d3c5fcb80b28bd88e94ef0e93ab8c548a0, reversing
    changes made to ffacbaf1c068c04ea0f63ada378a67822de84c12.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,9 +24,8 @@
                      <div class="col-md-6">
                         <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                        <div class="form-group row">
-
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Correo') }}</label>
+                     </div>
+                  </div>
 
                   <div class="form-group row">
                      <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
