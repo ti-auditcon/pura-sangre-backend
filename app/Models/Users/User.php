ecfcba0065d9c54773ee5c4abb09795151da613c
boryeira <jmanuel.jorquera@gmail.com>
2019-01-08 20:12:26 -0300

    birthdate bug

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -40,13 +40,13 @@ class User extends Authenticatable
     public function setBirthdateAttribute($value)
     {
         // dd(Carbon::parse($value)->format('Y-m-d'));
-        $this->attributes['birthdate'] = Carbon::parse($value)->format('Y-m-d');
+        $this->attributes['birthdate'] = Carbon::createFromFormat('d/m/Y', $value);
     }
 
     public function setSinceAttribute($value)
     {
         // dd(Carbon::parse($value)->format('Y-m-d'));
-        $this->attributes['since'] = Carbon::parse($value)->format('Y-m-d');
+        $this->attributes['since'] = Carbon::createFromFormat('d/m/Y', $value);
     }
 
     /**
