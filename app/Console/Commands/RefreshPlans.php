811d3f94cebc7b2c8c56964d81b806c543dc8373
unknown <raulberrios8@gmail.com>
2019-01-23 12:42:32 -0300

    Create RefreshPlans.php

---
+++ app/Console/Commands/RefreshPlans.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\Users\User;
+use App\Models\Plans\PlanUser;
+use Illuminate\Console\Command;
+
+class RefreshPlans extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'refresh:plans';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Loop over all plan_user and change the status of all plans with expired dates';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::all();
+        foreach ($users as $user) {
+            $plans = PlanUser::where('user_id', $user->id)->whereIn('plan_status_id', [1,3])->get();
+            foreach ($plans as $plan) {
+                if ($plan->plan_status_id == 1) {
+                    if (Carbon::parse($plan->finish_date) < today()) {
+                        $plan->plan_status_id = 4;
+                        $plan->save();
+                    }
+                }
+                if ($plan->plan_status_id == 3) {
+                    if (Carbon::parse($plan->start_date) <= today() && Carbon::parse($plan->finish_date) >= today()) {
+                        $plan->plan_status_id = 1;
+                        $plan->save();
+                    }
+                }
+            }
+            if ($user->actual_plan) {
+                $user->status_user_id = 1;
+            }else{
+                $user->status_user_id = 2;
+            }
+            $user->save();
+        }
+    }
+}
