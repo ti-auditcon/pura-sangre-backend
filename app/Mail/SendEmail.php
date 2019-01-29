c32d0c00da947cd2e3295461a7f0a789d3513ead
unknown <raulberrios8@gmail.com>
2019-01-29 13:54:45 -0300

    varios

---
+++ app/Mail/SendEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $demo;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($demo)
+    {
+        $this->demo = $demo;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('messages.template')
+                    ->subject($this->demo->subject)
+                    ->from('contacto@purasangrecrossfit.cl');
+    }
+}
