713c7859dd1731de3b77686abc930215b8df7f52
RaulEbc <raulberrios8@gmail.com>
2019-06-07 18:57:01 -0400

    advance

---
+++ app/Mail/SendEmailQueue.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendEmailQueue extends Mailable implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 3;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 120;
+
+    /**
+     * [$demo description]
+     * @var Collection
+     */
+    public $demo;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($demo)
+    {
+        $this->demo = $demo;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('messages.template')
+                    ->subject($this->demo->subject);
+    }
+}
