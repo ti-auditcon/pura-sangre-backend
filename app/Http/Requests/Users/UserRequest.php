cbd63daad15927dac4d5749d2259f97267276f25
Raul Berrios <raulberrios8@gmail.com>
2019-08-30 11:55:09 -0400

    Contact phone validation
    Fix validation to create a new user fails on contact_phone field non numeric

--- app/Http/Requests/Users/UserRequest.php
+++ app/Http/Requests/Users/UserRequest.php
@@ -27,7 +27,7 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
-        // dd($this->user);
+        // dd($this->contact_phone);
         switch ($this->method()){
             case 'POST': {
                 return [
@@ -36,11 +36,11 @@ class UserRequest extends FormRequest
                     'rut' => new RutUnique,
                     'email' => 'required|email|unique:users',
                     'phone' => $this->phone != null ? 'digits:8': '',
+                    'contact_phone' => $this->contact_phone ? 'digits:8' : ''
                 ];
             }
 
             case 'PUT': {
-                // dd(request()->all());
                 $case = $this->route('user')->email != $this->email ?
                         '|unique:users,email' :
                         null;
@@ -56,6 +56,7 @@ class UserRequest extends FormRequest
                     'image' => 'mimes:jpeg,png|max:1024',
                     'email' => $required.'|email'.$case,
                     'phone' => $this->phone != null ? 'digits:8': '',
+                    'contact_phone' => $this->contact_phone ? 'numeric' : ''
                 ];
            }
            default:break;
@@ -66,19 +67,19 @@ class UserRequest extends FormRequest
     * [messages description]
     * @return [array] [description]
     */
-   public function messages()
-   {
-     return [
-       'first_name.required' => 'Debe ingresar un nombre.',
-       'last_name.required' => 'Debe ingresar un apellido.',
-       'email.required' => 'Debe ingresar un e-mail.',
-       'email.email' => 'El formato del email es incorrecto.',
-       'email.unique' => 'El email ya ha sido tomado.',
-       'phone.digits' => 'El número de teléfono debe contener :digits dígitos.',
-       'phone.digits' => 'El número de teléfono debe contener :digits dígitos.',
-       'image.mimes' => 'El formato de imagen debe ser jpeg o png',
-       'image.max' => 'La imagen no debe se mas grande que 1 MB',
-     ];
-   }
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'Debe ingresar un nombre.',
+            'last_name.required' => 'Debe ingresar un apellido.',
+            'email.required' => 'Debe ingresar un e-mail.',
+            'email.email' => 'El formato del email es incorrecto.',
+            'email.unique' => 'El email ya ha sido tomado.',
+            'phone.digits' => 'El número de teléfono debe contener :digits dígitos de solo números.',
+            'image.mimes' => 'El formato de imagen debe ser jpeg o png',
+            'image.max' => 'La imagen no debe se mas grande que 1 MB',
+            'contact_phone.digits' => 'El campo Teléfono de contacto de emergencia debe contener :digits dígitos de solo números.'
+        ];
+    }
  }
 
