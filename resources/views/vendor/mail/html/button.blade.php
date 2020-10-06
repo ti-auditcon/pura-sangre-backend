08bddc04e156ffc31ac5cd39d61ae09b46fd10c4
unknown <raulberrios8@gmail.com>
2020-10-06 12:57:33 -0300

    =

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,6 +1,6 @@
-<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" style="">
+<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0">
     <tr>
-        <td align="center" s>
+        <td align="center">
             <table width="100%" border="0" cellpadding="0" cellspacing="0">
                 <tr>
                     <td align="center">
